071cfeacb7d661ba458afe037b82c687d8be1548
Ayu Lestari <ayulestarinasution848@gmail.com>
2023-02-22 22:11:51 +0700

    update migrata dan responsive

--- src/componets/Form.tsx
+++ src/componets/Form.tsx
@@ -1,6 +1,22 @@
-import * as React from 'react';
+import React, {useState} from 'react';
+import 'tailwindcss/tailwind.css';
 
-export default function Form( ) {
+ function Form( ) {
+
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+  
+    const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(event.target.value);
+    }
+  
+    const handlePassChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setPass(event.target.value);
+    }
+  
+    const handleButtonClick = () => {
+      alert(`SIGN IN SUCCES`);
+    }
     return (
         <div className='bg-white px-10 py-20 rounder-3xl border-2 border-gray-200'>
       
@@ -12,6 +28,8 @@ export default function Form( ) {
                 Email
             </label>
             <input
+            type="text" value={email}
+            onChange={handleEmailChange} 
             className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
             placeholder='enter your email' />
         </div>
@@ -20,9 +38,11 @@ export default function Form( ) {
                 Password
             </label>
             <input
+             type="text" value={pass} 
+             onChange={handlePassChange} 
             className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
-            placeholder='enter your password'
-            type="password"/>
+            placeholder='enter your password'/>
+           
         </div>
 <div className='mt-8 flex justify-betwenn items-center'>
     <div>
@@ -36,7 +56,7 @@ export default function Form( ) {
     
 </div>
 <div className='mt-8 flex  flex-col gap-y-4'>
-<button className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3  rounded-xl bg-violet-500 text-white text-lg font-bold'>sign in</button>
+<button  onClick={handleButtonClick} className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3  rounded-xl bg-violet-500 text-white text-lg font-bold'>sign in</button>
 <button>sign in with google</button>
 
     </div>
@@ -48,3 +68,4 @@ export default function Form( ) {
      </div>
     )
 }
+export default Form;
